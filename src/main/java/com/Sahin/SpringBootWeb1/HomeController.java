fb8a86c6656c1e4b1048aa1345c54aaea9e088d7
Sahin Islam <sahinislam878@gmail.com>
2025-07-19 19:35:52 +0530

    Progress: Completed RequestMapping Lecture from Udemy

--- src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
+++ src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
@@ -1,10 +1,12 @@
 package com.Sahin.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
     public String home(){
         System.out.println("Home Method Called!");
         return  "index.jsp";
