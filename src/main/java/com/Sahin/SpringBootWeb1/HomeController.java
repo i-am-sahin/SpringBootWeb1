b97cf75e0323c5b4e82200333734c08383ef8320
Sahin Islam <sahinislam878@gmail.com>
2025-07-20 13:30:11 +0530

    Progress: Completed Display Data on Result Page Lecture from Udemy

--- src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
+++ src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
@@ -1,6 +1,7 @@
 package com.Sahin.SpringBootWeb1;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,12 +16,13 @@ public class HomeController {
 
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
+    public String add(HttpServletRequest req, HttpSession session){
 
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
 
+        session.setAttribute("result",result);
         System.out.println(result);
         return "result.jsp";
     }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -5,8 +5,12 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
     <body>
-        <h2>Result Is : </h2>
 
+        <p>Data Fetched by Servlet and Session</p>
+        <h2>Result Is : <%= session.getAttribute("result") %> </h2>
+
+        <p>Data Fetched by JSTL(Part of JSP standard Library) </p>
+        <h2>Result Is : ${result}  </h2>
 
 
     </body>
