f76e72719129178496d08b2cd8a3954a7483198e
Sahin Islam <sahinislam878@gmail.com>
2025-07-20 13:54:34 +0530

    Progress: Completed Model Object Lecture from Udemy

--- src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
+++ src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
@@ -3,6 +3,7 @@ package com.Sahin.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,19 +13,20 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("Home Method Called!");
-        return  "index.jsp";
+        return  "index";
     }
 
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, @RequestParam("num2")int num2 , HttpSession session){
+    public String add(@RequestParam("num1") int num, @RequestParam("num2")int num2 , Model model){
 
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
 //        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num + num2;
 
-        session.setAttribute("result",result);
+//        session.setAttribute("result",result);
+        model.addAttribute("result",result);
         System.out.println(result);
-        return "result.jsp";
+        return "result";
     }
 }
