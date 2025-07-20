05d565b0eb9a5c27c6869458698d381bf6cd8d15
Sahin Islam <sahinislam878@gmail.com>
2025-07-20 13:08:56 +0530

    Progress: Completed Accepting Data the servlet way Lecture from Udemy

--- src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
+++ src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
@@ -1,5 +1,6 @@
 package com.Sahin.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,17 @@ public class HomeController {
     public String home(){
         System.out.println("Home Method Called!");
         return  "index.jsp";
+    }
+
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req){
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+
+        System.out.println(result);
+        return "result.jsp";
     }
 }
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,15 @@
+<%@page language="java" %>
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    <body>
+        <h2>Result Is : </h2>
+
+
+
+    </body>
+</html>
+
+
