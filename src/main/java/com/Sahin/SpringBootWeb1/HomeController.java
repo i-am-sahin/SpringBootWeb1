6ea16c0d56cb35bd989590d78d2fe9b4e705cfe5
Sahin Islam <sahinislam878@gmail.com>
2025-07-21 13:29:38 +0530

    Progress: Completed ModelAndView Lecture from Udemy

--- src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
+++ src/main/java/com/Sahin/SpringBootWeb1/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -18,15 +19,20 @@ public class HomeController {
 
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, @RequestParam("num2")int num2 , Model model){
+    public ModelAndView add(@RequestParam("num1") int num, @RequestParam("num2")int num2 , ModelAndView mv){
 
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
 //        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num + num2;
 
 //        session.setAttribute("result",result);
-        model.addAttribute("result",result);
+//        model.addAttribute("result",result);
+
+        mv.addObject("result",result);
+        mv.setViewName("result");
+
+
         System.out.println(result);
-        return "result";
+       return mv;
     }
 }
--- src/main/webapp/views/result.jsp
+++ src/main/webapp/views/result.jsp
@@ -6,8 +6,7 @@
 </head>
     <body>
 
-        <p>Data Fetched by Servlet and Session</p>
-        <h2>Result Is : <%= session.getAttribute("result") %> </h2>
+
 
         <p>Data Fetched by JSTL(Part of JSP standard Library) </p>
         <h2>Result Is : ${result}  </h2>
